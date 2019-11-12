dfa6c21387a39d1608758cb99db4df6682c8573f
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-11-12 01:53:28 -0300

    primeira rota

--- config/web.php
+++ config/web.php
@@ -47,7 +47,10 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'pluralize' => false,
+                    'controller' => 'api/default']
             ],
         ],
     ],
---
+++ views/site/feed.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <h1>Feed de notícias via Rest API</h1>
+
+    </div>
+</div>
